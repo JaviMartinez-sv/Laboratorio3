6e1efb5e75deb97fee6864b327e08a51b2ef1cc9
Javier Martinez <javijog@gmail.com>
2021-02-09 19:18:39 +0100

    Ejercicio3

---
+++ ejecuciondocker.bat
@@ -0,0 +1,3 @@
+docker pull mcr.microsoft.com/mssql/server:2019-latest &
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Pa$$w0rdSQL" -p 1501:1433 --name sqltestcontainer -d mcr.microsoft.com/mssql/server:2019-latest &
+docker ps
